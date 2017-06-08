a95440a54263e967fceeb10f8e4ee98bad2c273e
elenamorton <elena.uleia@gmail.com>
2017-06-08 12:14:48 +0100

    Adding displayWeather function.

--- src/interface.js
+++ src/interface.js
@@ -30,22 +30,28 @@ $(document).ready(function(){
     updateTemperature();
   });
 
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-  $('#current-temperature').text(data.main.temp);
-})
-$('#current-city').change(function() {
-  var city = $('#current-city').val();
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-    $('#current-temperature').text(data.main.temp);
-  })
-  $('#selected-city').text($('#current-city').name());
-})
+  displayWeather("London");
+    
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    displayWeather(city);
+  });
 
 
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.getCurrentEnergyUsage());
   }
-
-
+ 
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' ;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+      
+    $.get(url + city + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);  
+    });
+  }
+    
+    
 });
